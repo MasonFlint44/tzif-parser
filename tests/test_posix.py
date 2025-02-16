cccc37d0a11ffc1ae7e26b14ef0a1e8761ea4a9d
Mason Flint <masonflint44@gmail.com>
2025-02-16 23:00:48 +0000

    added more tests around dst times

--- tests/test_posix.py
+++ tests/test_posix.py
@@ -10,6 +10,10 @@ from tzif_parser.posix import PosixTzDateTime
     [
         (PosixTzDateTime(6, 1, 1, 0, 0, 0), 2025, datetime(2025, 6, 2, 0, 0, 0)),
         (PosixTzDateTime(1, 1, 0, 0, 0, 0), 2025, datetime(2025, 1, 5, 0, 0, 0)),
+        (PosixTzDateTime(3, 2, 0, 2, 0, 0), 2025, datetime(2025, 3, 9, 2, 0, 0)),
+        (PosixTzDateTime(11, 1, 0, 2, 0, 0), 2025, datetime(2025, 11, 2, 2, 0, 0)),
+        (PosixTzDateTime(3, 2, 0, 2, 0, 0), 2026, datetime(2026, 3, 8, 2, 0, 0)),
+        (PosixTzDateTime(11, 1, 0, 2, 0, 0), 2026, datetime(2026, 11, 1, 2, 0, 0)),
     ],
 )
 def test_posix_tz_datetime_to_datetime(posix_datetime, year, expected):
